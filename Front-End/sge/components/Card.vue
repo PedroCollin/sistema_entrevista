0ad08e87e50e7629d2fc4ac153e11968e9de3be3
Francisco Vitor (CaP/ETS) <frv1ca@bosch.com>
2022-04-19 10:47:18 -0300

    att card

--- Front-End/sge/components/Card.vue
+++ Front-End/sge/components/Card.vue
@@ -1,34 +1,19 @@
 <template>
-    <div>
-        <Card style="width: 25rem; margin-bottom: 2em">
-            <template #title>
-                Simple Card
-            </template>
-            <template #content>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
-                quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!</p>
-            </template>
-        </Card>
-
-        <Card style="width: 25em">
-            <template #header>
-                <img src="https://www.primefaces.org/wp-content/uploads/2020/02/primefacesorg-primevue-2020.png" style="height: 15rem" />
-            </template>
-            <template #title>
-                Advanced Card
-            </template>
-            <template #subtitle>
-                Card subtitle
-            </template>
-            <template #content>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
-                quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!</p>
-            </template>
-            <template #footer>
-                <Button icon="pi pi-check" label="Save" />
-                <Button icon="pi pi-times" label="Cancel" class="p-button-secondary" style="margin-left: .5em" />
-            </template>
-        </Card>
+    <div class="container">
+        <template class="header">
+            <img alt="user header" src="demo/images/usercard.png">
+        </template>
+        <template class="title">
+            Advanced Card
+        </template>
+        <template class="content">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
+            quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+        </template>
+        <template class="footer">
+            <Button icon="pi pi-check" label="Save" />
+            <Button icon="pi pi-times" label="Cancel" class="p-button-secondary" style="margin-left: .5em" />
+        </template>
     </div>
 </template>
 
@@ -37,16 +22,9 @@ import Card from 'primevue/card';
 
 export default {
     name: "Card",
-
-    components: {
-        Card:Card,
-    },
 }
 </script>
 
 <style>
-p {
-    line-height: 1.5;
-    margin: 0;
-}
+
 </style>
