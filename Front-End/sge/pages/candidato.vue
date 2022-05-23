bce314b65c3ac7f45453e6d440a304fc76c38636
Collin Pedro (CaP/ETS) <cop4ca@bosch.com>
2022-05-23 16:45:58 -0300

    finish screen candidatos

--- Front-End/sge/pages/candidato.vue
+++ Front-End/sge/pages/candidato.vue
@@ -1,13 +1,72 @@
 <template>
  
     <div class="container">
-      <div class="tabela">
+      <div class="tabela" v-if="dataLoaded == true">
+          <DataTable :value="candidate" responsiveLayout="scroll">
+              
+
+          
+          <Column field="CANDIDATO" header="CANDIDATO">
+                <template #body="slotProps">
+                  <img src="image1.jpg" alt="imagem candidato" width="100px" height="100px"/>
+              </template>
+          </Column>
+          <Column field="name" header="NOME DO CANDIDATO">
+            <template #body="slotProps">
+              <p style="text-align:center; width:100%">
+                {{ slotProps.data.candidato.nome.toUpperCase() }}
+              </p>
+          </template>
+          </Column>
+          <Column header="CIDADE">
+          <template #body="slotProps">
+              <p style="text-align:center; width:100%">
+                {{ slotProps.data.candidato.cidade.nome.toUpperCase() }}
+              </p>
+          </template>
+          </Column>
+          <Column field="email" header="EMAIL">
+                    <template #body="slotProps">
+              <p style="text-align:center; width:100%">
+                {{ slotProps.data.candidato.email.toUpperCase() }}
+              </p>
+          </template>
+          </Column>
+          <Column field="rg" header="RG">
+                              <template #body="slotProps">
+              <p style="text-align:center; width:100%">
+                {{ slotProps.data.candidato.rg.toUpperCase() }}
+              </p>
+          </template>
+          </Column>
+          <Column field="cpf" header="CPF">
+                              <template #body="slotProps">
+              <p style="text-align:center; width:100%">
+                {{ slotProps.data.candidato.cpf.toUpperCase() }}
+              </p>
+          </template>
+          </Column>
+        <Column         
+          field=" "
+          header=" "
+          class="columnEmpty"
+        >
+          <template #body="slotProps">
+           <center>
+              <p style="text-align:center; width:1%">
+                
+             </p>
+           </center>
+          </template>
+        </Column>
+
+      </DataTable>
       <DataTable
         :value="candidates"
         rowGroupMode="subheader"
-        groupRowsBy="usuario.nome"
+        groupRowsBy="usuario"
         sortMode="single"
-        sortField="usuario.name"
+        sortField="usuario"
         :sortOrder="1"
         responsiveLayout="scroll"
         :expandableRowGroups="true"
@@ -17,7 +76,7 @@
       >
         <Column field="DINÂMICA" header="DINÂMICA">
           <template #body="slotProps">
-            <p style="text-align:center; width:100%">
+            <p style="text-align:center; width:100%;">
               {{ slotProps.data.vagaDinamica.dinamica.titulo }}
             </p>
           </template>
@@ -36,13 +95,31 @@
            </center>
           </template>
         </Column>
+        
 
+        <Column         
+          field=" "
+          header=" "
+          class="columnEmpty"
+        >
+          <template #body="slotProps">
+           <center>
+              <p style="text-align:center; width:1%">
+                
+             </p>
+           </center>
+          </template>
+        </Column>
+        
+ 
         <template #groupheader="slotProps">
           <strong
             ><span class="image-text">
               {{ slotProps.data.usuario.nome }}
-            </span></strong
-          >
+            </span></strong>
+         
+            <!-- <hr class="hrteste"> -->
+
         </template>
 
         <template #body="slotProps">
@@ -52,11 +129,11 @@
         </template>
 
         <template #groupfooter="slotProps">
-
+              
           <div class="ultima">
             <td colspan="4" style="text-align: left">OBSERVAÇÔES :</td> 
               <div class="final">
-              <Textarea v-model="observacao" :autoResize="true" rows="5" cols="30" disabled/>
+              <Textarea v-model="slotProps.data.observacao" :autoResize="true" rows="5" cols="30" disabled/>
               </div>
             <div class="final">
             <td colspan="4" style="text-align: right">NOTA FINAL :</td>
@@ -69,6 +146,8 @@
           </div>
 
         </template> 
+       
+
       </DataTable>
       </div>
        <div class="master">
@@ -83,23 +162,31 @@ export default {
    data() {
     return {
       list_criterios: [],
+      usuario:[],
       candidates: [],
+      candidate: [],
       expandedRowGroups: null,
       observacao: [],
+      dataLoaded: false,
     }
   },
   created() {
     this.$axios
-      .get('http://127.0.0.1:8000/api/main/respostaDinamica/1')
+      .get('http://127.0.0.1:8000/api/main/respostaDinamica/2')
       .then((response) => {
-        // console.log(response.data[0])
-        this.candidates.push(response.data[2])
+        console.log('TESTESTESTES',response.data)
+        this.candidates = response.data
         console.log('Candidates', this.candidates)
-        this.observacao.push(response.data[2].observacao)
+        this.candidate.push(response.data[0])
+        console.log('Candidate', this.candidate)
+        response.data.map((observacao)=>{
+          this.observacao.push(observacao.observacao)
+        })
+        // this.observacao.push(response.data[2].observacao)
         console.log('observacao', this.observacao)
-        this.list_criterios = Object.keys(response.data[2].list_criterios)
+        this.list_criterios = Object.keys(response.data[0].list_criterios)
         console.log('criterios', this.list_criterios)
-
+        this.dataLoaded = true;
       })
   },
   methods: {
